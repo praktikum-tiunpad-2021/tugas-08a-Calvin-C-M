d5c90de6324cb25e122cd3aab60f4bc06f454bcd
Calvin <montolalu28@gmail.com>
2021-04-14 23:10:06 +0700

    Commit #1

--- main.cpp
+++ main.cpp
@@ -12,6 +12,21 @@ using namespace strukdat::priority_queue;
 
 int main()
 {
+  auto Q = new_queue<int>();
+  enqueue(Q, 1, 5);
+  enqueue(Q, 2, 3);
+  enqueue(Q, 3, 7);
+  enqueue(Q, 4, 5);
+  enqueue(Q, 5, 1);
+
+  // dequeue(Q);
+
+  ElementPtr<int> curr = Q.head;
+  while(curr != nullptr)
+  {
+    std::cout << curr->data << " : " << curr->priority << '\n';
+    curr = curr->next;
+  }
   
   return 0;
 }
